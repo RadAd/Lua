9bc8a08c27a70fd609f0edd1da16648748fac599
RadAd <adamgates84+github@gmail.com>
2020-04-17 17:08:10 +1000

    Added lfs to package

--- package.bat
+++ package.bat
@@ -10,5 +10,5 @@ goto :eof
 :pack
 del Lua_%1.zip
 del LuaDev_%1.zip
-zip -j Lua_%1.zip Bin\Release%1\Lua.exe Bin\Release%1\Lua.dll || exit /b 1
+zip -j Lua_%1.zip Bin\Release%1\Lua.exe Bin\Release%1\Lua.dll Bin\Release%1\lfs.dll || exit /b 1
 zip -j LuaDev_%1.zip Bin\Release%1\Lua.dll Bin\Release%1\Lua.lib lua\lauxlib.h lua\lua.h lua\luaconf.h lua\lualib.h || exit /b 1
