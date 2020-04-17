45379078c77be648eb309437bdd2b42fa2550e58
RadAd <adamgates84+github@gmail.com>
2020-04-17 15:57:20 +1000

    First version

---
+++ package.bat
@@ -0,0 +1,12 @@
+setlocal
+prompt $G$S
+
+call :pack Win32 || exit /b 1
+call :pack x64 || exit /b 1
+goto :eof
+
+:pack
+del Lua_%1.zip
+del LuaDev_%1.zip
+zip -j Lua_%1.zip Bin\Release%1\Lua.exe Bin\Release%1\Lua.dll || exit /b 1
+zip -j LuaDev_%1.zip Bin\Release%1\Lua.dll Bin\Release%1\Lua.lib lua\lauxlib.h lua\lua.h lua\luaconf.h lua\lualib.h || exit /b 1
