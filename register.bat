ba175e1538c819937c89db7dfb33eb6d9e7b8b1f
RadAd <adamgates84+github@gmail.com>
2022-01-25 20:48:19 +1100

    Added register.bat

---
+++ register.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+set LUA="%~dp0lua.exe"
+if not exist %LUA% echo Lua.exe not found && exit /b 1
+
+set CMD=%LUA% %%1 %%*
+
+rem echo --- %CMD%
+set CMD=!CMD:^%%=^%%^%%!
+set CMD=!CMD:"="""!
+rem echo --- %CMD%
+
+call :register .lua RadLua "%CMD%"
+goto :eof
+
+:register
+if not "%~4" == "" (echo Too manu arguments && exit /b 1)
+reg add "HKEY_CURRENT_USER\SOFTWARE\Classes\%~1" /ve /f /d %2
+reg add "HKEY_CURRENT_USER\SOFTWARE\Classes\%~2\shell\open\command" /ve /f /d %3
+goto :eof
